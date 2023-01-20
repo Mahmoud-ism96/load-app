63961911a1206d4c4383e35f2d4be8ef851e815c
mahmoud.ahm.ism <mah.ahm.ism@gmail.com>
2023-01-20 03:22:21 +0200

    Minor bug fixes

--- app/src/main/java/com/udacity/DetailActivity.kt
+++ app/src/main/java/com/udacity/DetailActivity.kt
@@ -1,7 +1,9 @@
 package com.udacity
 
+import android.app.NotificationManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_detail.*
 import kotlinx.android.synthetic.main.content_detail.*
 
@@ -12,11 +14,17 @@ class DetailActivity : AppCompatActivity() {
         setContentView(R.layout.activity_detail)
         setSupportActionBar(toolbar)
 
-        val intent = intent
+        cancelAll()
 
+        val intent = intent
 
         val name = intent.getStringExtra("fileName")
         val stat = intent.getStringExtra("status")
+
+        if (stat == "Success")
+            status.setTextColor(resources.getColor(R.color.success))
+        else
+            status.setTextColor(resources.getColor(R.color.fail))
 
         file_name.text = name
         status.text = stat
@@ -24,8 +32,14 @@ class DetailActivity : AppCompatActivity() {
         ok_button.setOnClickListener {
             finish()
         }
+    }
 
+    private fun cancelAll() {
+        val notificationManager = ContextCompat.getSystemService(
+            this, NotificationManager::class.java
+        ) as NotificationManager
 
+        notificationManager.cancelAll()
     }
 
 }
